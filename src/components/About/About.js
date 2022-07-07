dfb7e6b542efd85d7cce0853f61c081f81bc5c19
Bill Zappasodi <billyzapp@gmail.com>
2022-07-07 07:51:36 -0400

    update to about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import {Col, Row} from "react-bootstrap";
 
 
-const About = () =>{
+const About = () => {
 
-    let aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    let aboutText = "Creativity is the motivation. Within a structure or not, I always try to make the shot say something to me. I find many parallels between Photography, Guitar playing, and Programming. My attempt here is to display the multi-level process involved with being creative in Photographic endeavors. Being confined to a given set of rules or constraints, how far off the path does a creative individual go in order to achieve results that are worthwhile to the end user as well as the developer. Here I will add my images to the various projects I am involved with in these areas.";
 
-
-    return(
-        <div>
-            {aboutText}
-        </div>
+    return (
+        <>
+            <Row><h2>About</h2></Row>
+            <Row>
+                <Col xs={2}/>
+                <Col xs={8} className="aboutText text-link">{aboutText}</Col>
+                <Col xs={2}/>
+            </Row>
+        </>
     )
 }
 
